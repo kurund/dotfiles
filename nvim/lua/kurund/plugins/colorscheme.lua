58bb1b8406acc195befd048ed6d7614ec5b5e859
Kurund Jalmi <kurundjalmi@thirdsectordesign.org>
2024-11-21 11:01:17 +0000

    minor clean up

--- nvim/lua/kurund/plugins/colorscheme.lua
+++ nvim/lua/kurund/plugins/colorscheme.lua
@@ -1,58 +1,41 @@
 return {
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+  {
+    -- theme switcher
+    'zaldih/themery.nvim',
+    lazy = false,
+    config = function()
+      require('themery').setup {
+        themes = { 'tokyonight', 'nord', 'rose-pine', 'catppuccin', 'onedark' }, -- Your list of installed colorschemes.
+        livePreview = true, -- Apply theme while picking. Default to true.
+      }
     end,
-    -- opts = {
-    --   transparent = true,
-    --   styles = {
-    --     sidebars = 'transparent',
-    --     floats = 'transparent',
-    --   },
-    -- },
+  },
+
+  -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
+    'folke/tokyonight.nvim',
+    name = 'tokyonight',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
   },
 
   {
     'gbprod/nord.nvim',
-    lazy = false,
+    name = 'nord',
     priority = 1000,
-    config = function()
-      local settransparancy = function()
-        if (vim.uv or vim.loop).os_uname().sysname == 'Darwin' then
-          return false
-        end
-        return true
-      end
-
-      require('nord').setup {
-        transparent = settransparancy(),
-      }
-      -- vim.cmd.colorscheme 'nord'
-    end,
   },
 
   {
     'rose-pine/neovim',
-    priority = 1000,
     name = 'rose-pine',
+    priority = 1000,
     config = function()
       require('rose-pine').setup {
         styles = {
           bold = true,
           italic = false,
-          transparent = true,
         },
       }
     end,
@@ -62,5 +45,16 @@ return {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
+  },
+
+  {
+    'navarasu/onedark.nvim',
+    name = 'onedark',
+    priority = 1000,
+    config = function()
+      require('onedark').setup {
+        style = 'darker',
+      }
+    end,
   },
 }
