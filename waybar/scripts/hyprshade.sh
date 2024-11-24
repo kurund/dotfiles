e9bfed1c6471c5a6b91fa34db5bdcd8afa930f5f
Kurund Jalmi <kurundjalmi@thirdsectordesign.org>
2024-11-24 18:44:27 +0000

    addd waybar config

---
+++ waybar/scripts/hyprshade.sh
@@ -0,0 +1,20 @@
+    # Toggle Hyprshade based on the selected filter
+    hyprshade_filter="blue-light-filter-50"
+
+    # Toggle Hyprshade
+    if [ "$hyprshade_filter" != "off" ] ;then
+        if [ -z $(hyprshade current) ] ;then
+            echo ":: hyprshade is not running"
+            hyprshade on $hyprshade_filter
+            notify-send "Hyprshade activated" "with $(hyprshade current)"
+            echo ":: hyprshade started with $(hyprshade current)"
+        else
+            notify-send "Hyprshade deactivated"
+            echo ":: Current hyprshade $(hyprshade current)"
+            echo ":: Switching hyprshade off"
+            hyprshade off
+        fi
+    else
+        hyprshade off
+        echo ":: hyprshade turned off"
+    fi
---
+++ waybar/scripts/launch.sh
@@ -0,0 +1,8 @@
+# kill existing waybar instance
+killall waybar
+pkill waybar
+sleep 0.5
+
+waybar &
+
+
